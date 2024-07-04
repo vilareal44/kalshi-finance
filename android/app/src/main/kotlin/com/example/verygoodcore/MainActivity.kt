c9e34cc033f86c74f908e76c8d6b27e3b93f7213
Henrique Vila Real <henrique@codebase.com.br>
2024-07-04 19:02:09 -0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.kalshi_finance
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:kalshi_finance/app/app.dart';
+import 'package:kalshi_finance/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
