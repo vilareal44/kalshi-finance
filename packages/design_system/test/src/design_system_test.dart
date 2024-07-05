ad74ce55d9303d831353a21d97c3ccaae6a751f2
Henrique Vila Real <henrique@codebase.com.br>
2024-07-05 17:21:54 -0300

    tests: removing unused tests

--- packages/design_system/test/src/design_system_test.dart
+++ packages/design_system/test/src/design_system_test.dart
@@ -1,12 +1,3 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'package:design_system/design_system.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  group('DesignSystem', () {
-    test('can be instantiated', () {
-      expect(DesignSystem(), isNotNull);
-    });
-  });
-}
+void main() {}
--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,3 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:kalshi_finance/app/app.dart';
-import 'package:kalshi_finance/home/counter.dart';
 
-void main() {
-  group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
-      await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
-    });
-  });
-}
+void main() {}
